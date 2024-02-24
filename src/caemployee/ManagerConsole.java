e565e5df19e8de3b0d5a66aef8d1ce0f89038ee4
OisinRyan22 <sba23149@student.cct.ie>
2024-02-24 15:22:39 +0000

    Manager console
    Added username and password
    Imported scanner

---
+++ src/caemployee/ManagerConsole.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caemployee;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ocean
+ */
+public class ManagerConsole {
+    private static final String UserName = "Gnomeo";
+    private static final String Password = "smurf";
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+}
+}
