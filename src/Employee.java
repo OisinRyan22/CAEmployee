b3b1148cf4fa4b545f45007ca7d84ec6cefafb24
OisinRyan22 <sba23149@student.cct.ie>
2024-02-24 12:41:29 +0000

    Made .java file called Employee

---
+++ src/Employee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ocean
+ */
+public class Employee {
+    private String name;
+    private String email;
+    private int empNum;
+    private static int nextEmpNum = 1;
+    
+}
