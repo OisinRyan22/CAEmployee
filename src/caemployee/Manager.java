3dfe6d750d45710d9bd57c74221443d4780315d9
OisinRyan22 <sba23149@student.cct.ie>
2024-02-27 19:58:37 +0000

    manager.java
    Filling out required files and fixing original code

--- src/caemployee/Manager.java
+++ src/caemployee/Manager.java
@@ -8,15 +8,31 @@ package caemployee;
  *
  * @author ocean
  */
-public class Manager {
+public class Manager extends Employee {
+
     private String username;
     private String password;
-    
+
     public Manager(String name, String email, String username, String password) {
         super(name, email);
         this.username = username;
         this.password = password;
     }
-    
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+
     }
 }
